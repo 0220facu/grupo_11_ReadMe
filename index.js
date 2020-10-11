741f677913e140840518bf14ecec8efe275d9f28
0220facu <facu_r@outlook.com>
2020-10-11 17:01:35 -0300

    index.html agregue el header

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+
+
 app.listen(3000, ()=> {
     console.log('Servidor Levantado. http//localhost:3000');
 });
