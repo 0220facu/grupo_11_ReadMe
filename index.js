2812afd5a2ad5893f73c48c0aaa27f7dc844d57b
0220facu <facu_r@outlook.com>
2020-10-11 14:17:28 -0300

    armé los html

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.listen(3000, ()=> {
+    console.log('Servidor Levantado. http//localhost:3000');
+});
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/html/index.html');
+});
