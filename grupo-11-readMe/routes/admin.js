5a8408cbd0311194204ea4fbb61fd083af0fd15a
0220facu <facu_r@outlook.com>
2020-12-17 15:08:53 -0300

    arregle fallos de sprint anterior

--- grupo-11-readMe/routes/admin.js
+++ grupo-11-readMe/routes/admin.js
@@ -1,23 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const adminController = require('../controllers/adminController');
-const multer  = require('multer');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, __dirname + '/../public/images')
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname)
-    }
-  })
-
-  const upload = multer({ storage: storage })
-
-router.get('/',adminController.crear);
-router.get('/edit/:id',adminController.editar);
-router.put('/edit/:id',adminController.modificar);
-router.post('/crear',upload.any(), adminController.subir);
-router.delete('/delete/:id',adminController.eliminar);
 
 module.exports = router;
