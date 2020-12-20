248eb80961b13f7b22d2a188c267e9d2cc52f78f
juansenupieri <juansenupieri1973@gmail.com>
2020-12-20 17:24:53 -0300

    middlewares

--- grupo-11-readMe/routes/users.js
+++ grupo-11-readMe/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/usersController');
 const multer  = require('multer');
-
+const guest = require('../middelwares/guest') ;
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, __dirname + '/../public/images/fotos_de_perfil')
@@ -15,11 +15,11 @@ const storage = multer.diskStorage({
   const upload = multer({ storage: storage })
 
 
-router.get('/login',usersController.login );
+router.get('/login', guest ,usersController.login );
 
 router.post('/login',usersController.ingresar );
 
-router.get('/register',usersController.register );
+router.get('/register', guest, usersController.register );
 
 router.post('/register',upload.any(),usersController.crear );
 
