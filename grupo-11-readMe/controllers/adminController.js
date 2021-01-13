24c935b4799f4fcad37a98a8293a9312a6012d2d
0220facu <facu_r@outlook.com>
2021-01-12 22:20:07 -0300

    set categories

--- grupo-11-readMe/controllers/adminController.js
+++ grupo-11-readMe/controllers/adminController.js
@@ -3,21 +3,6 @@ const path = require( "path") ;
 const db = require('../database/models')
 
 
-function getAllProducts(){
-	const productsFilePath = path.join(__dirname, '../data/productos.json');
-    return JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-}
-function writeProducts(productsToSave){
-	const productsToStringify = JSON.stringify(productsToSave, null, ' ');
-	return fs.writeFileSync('./data/productos.json', productsToStringify);
-	
-}
-
-function generateNewId(){
-	const products = getAllProducts();
-	return products.pop().id + 1;
-}
-
 
 const controller = {
     
